94401b70a99587c1d37bd475149487aa4705c1f8
amr <a.mas.rodriguez@gmail.com>
2023-12-15 21:44:52 +0100

    community actions user actions an user profile

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -4,6 +4,8 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 import Community from "../models/community.model";
+import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 type UpdateUserPayload = {
   userId: string;
@@ -50,5 +52,124 @@ export async function fetchUser(userId: string | null) {
     });
   } catch (error) {
     throw new Error(`Failed to fetch ussr: ${(error as Error).message}`);
+  }
+}
+
+export async function fetchuserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads authored by the user with the given userId
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: [
+        {
+          path: "community",
+          model: Community,
+          select: "name id image _id",
+        },
+        {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "name image id",
+          },
+        },
+      ],
+    });
+
+    return threads;
+  } catch (error) {
+    console.error("Error fetching user thread: ", error);
+    throw error;
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
+
+    // Calculate the number of Users to skip based on the page number and the offset
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Create a case-insensitive regular expression for the provided search expression
+    const regex = new RegExp(searchString, "i");
+
+    // Create an initial query object to filter Users
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId }, // exclude current user
+    };
+
+    // if the search string is not empty add the $or operator to match existing Users
+    if (searchString.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    // Define the sort options for the fetched users based on created field
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    // Count the total number of users that match the sesarch criteria
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    // Check if there are more users beyond the current page.
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error) {
+    console.error("Error fetching users: ", error);
+    throw error;
+  }
+}
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    // find all threads created by the user
+    const userThreads = await Thread.find({ author: userId });
+
+    // Collect all the child thread ids (replies) from the 'children' field on each user thread
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concact(userThread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }, // Exclude threads authored by the same user
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error) {
+    console.error("Failed fetching user activity", error);
+    throw error;
   }
 }
