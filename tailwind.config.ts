02d17d34dd919a914cd1c4c2cdea3632b438eec3
amr <dquijote22@gmail.com>
2024-02-03 14:58:41 +0100

    Start reworking on my own

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+import { withUt } from "uploadthing/tw";
 
-const config: Config = {
+module.exports = withUt({
   darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     container: {
@@ -15,186 +17,66 @@ const config: Config = {
         "2xl": "1400px",
       },
     },
-    fontSize: {
-      "heading1-bold": [
-        "36px",
-        {
-          lineHeight: "140%",
-          fontWeight: "700",
-        },
-      ],
-      "heading1-semibold": [
-        "36px",
-        {
-          lineHeight: "140%",
-          fontWeight: "600",
-        },
-      ],
-      "heading2-bold": [
-        "30px",
-        {
-          lineHeight: "140%",
-          fontWeight: "700",
-        },
-      ],
-      "heading2-semibold": [
-        "30px",
-        {
-          lineHeight: "140%",
-          fontWeight: "600",
-        },
-      ],
-      "heading3-bold": [
-        "24px",
-        {
-          lineHeight: "140%",
-          fontWeight: "700",
-        },
-      ],
-      "heading4-medium": [
-        "20px",
-        {
-          lineHeight: "140%",
-          fontWeight: "500",
-        },
-      ],
-      "body-bold": [
-        "18px",
-        {
-          lineHeight: "140%",
-          fontWeight: "700",
-        },
-      ],
-      "body-semibold": [
-        "18px",
-        {
-          lineHeight: "140%",
-          fontWeight: "600",
-        },
-      ],
-      "body-medium": [
-        "18px",
-        {
-          lineHeight: "140%",
-          fontWeight: "500",
-        },
-      ],
-      "body-normal": [
-        "18px",
-        {
-          lineHeight: "140%",
-          fontWeight: "400",
-        },
-      ],
-      "body1-bold": [
-        "18px",
-        {
-          lineHeight: "140%",
-          fontWeight: "700",
-        },
-      ],
-      "base-regular": [
-        "16px",
-        {
-          lineHeight: "140%",
-          fontWeight: "400",
-        },
-      ],
-      "base-medium": [
-        "16px",
-        {
-          lineHeight: "140%",
-          fontWeight: "500",
-        },
-      ],
-      "base-semibold": [
-        "16px",
-        {
-          lineHeight: "140%",
-          fontWeight: "600",
-        },
-      ],
-      "base1-semibold": [
-        "16px",
-        {
-          lineHeight: "140%",
-          fontWeight: "600",
-        },
-      ],
-      "small-regular": [
-        "14px",
-        {
-          lineHeight: "140%",
-          fontWeight: "400",
-        },
-      ],
-      "small-medium": [
-        "14px",
-        {
-          lineHeight: "140%",
-          fontWeight: "500",
-        },
-      ],
-      "small-semibold": [
-        "14px",
-        {
-          lineHeight: "140%",
-          fontWeight: "600",
-        },
-      ],
-      "subtle-medium": [
-        "12px",
-        {
-          lineHeight: "16px",
-          fontWeight: "500",
-        },
-      ],
-      "subtle-semibold": [
-        "12px",
-        {
-          lineHeight: "16px",
-          fontWeight: "600",
-        },
-      ],
-      "tiny-medium": [
-        "10px",
-        {
-          lineHeight: "140%",
-          fontWeight: "500",
-        },
-      ],
-      "x-small-semibold": [
-        "7px",
-        {
-          lineHeight: "9.318px",
-          fontWeight: "600",
-        },
-      ],
-    },
     extend: {
       colors: {
-        "primary-500": "#877EFF",
-        "secondary-500": "#FFB620",
-        blue: "#0095F6",
-        "logout-btn": "#FF5A5A",
-        "navbar-menu": "rgba(16,16,18, 0.6)",
-        "dark-1": "#000000",
-        "dark-2": "#121417",
-        "dark-3": "#101012",
-        "dark-4": "#1F1F22",
-        "light-1": "#FFFFFF",
-        "light-2": "#EFEFEF",
-        "light-3": "#7878A3",
-        "light-4": "#5C5C7B",
-        "gray-1": "#697C89",
-        glassmorphism: "rgba(16, 16, 18, 0.6)",
+        primary: {
+          500: "#624CF5",
+          50: " #F6F8FD",
+          DEFAULT: "#624CF5",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        coral: {
+          500: "#15BF59",
+        },
+
+        grey: {
+          600: "#545454", // Subdued - color name in figma
+          500: "#757575",
+          400: "#AFAFAF", // Disabled - color name in figma
+          50: "#F6F6F6", // White Grey - color name in figma
+        },
+        black: "#000000",
+        white: "#FFFFFF",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        foreground: "hsl(var(--foreground))",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
-      boxShadow: {
-        "count-badge": "0px 0px 6px 2px rgba(219, 188, 159, 0.30)",
-        "groups-sidebar": "-30px 0px 50px 9px rgba(28, 28, 31, 0.5)",
+      fontFamily: {
+        poppins: ["var(--font-poppins)"],
       },
-      screens: {
-        xs: "400px",
+      backgroundImage: {
+        "dotted-pattern": "url('/assets/images/dotted-pattern.png')",
+        "hero-img": "url('/assets/images/hero.png')",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
         "accordion-down": {
@@ -213,5 +95,4 @@ const config: Config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
-export default config;
+});
